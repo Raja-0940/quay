4899deac695651de2fc75e6d574cc12b415b64f5
Rajakumar Battula <rkb0940@gmail.com>
2023-08-31 20:03:29 +0530

    Replaced ubuntu:20.04 image with python:3.9
    Signed-off-by: Rajakumar Battula <rkb0940@gmail.com>

--- hack/verify-requirements.sh
+++ hack/verify-requirements.sh
@@ -7,7 +7,7 @@ trap 'rc=$?; rm -rf "$TMP"; exit $rc' EXIT
 
 sed -n 's/ *#.*//; s/\[[^]]*\]//g; s/^pip==.*/pip/; /./p' ./requirements.txt | sort >"$TMP_REQUIREMENTS"
 
-pip install --target="$TMP_PACKAGESDIR" -r ./requirements.txt --ignore-installed
+pip install --target="$TMP_PACKAGESDIR" -r ./requirements.txt --ignore-installed cryptography
 pip freeze --path="$TMP_PACKAGESDIR" --all | sed 's/^pip==.*/pip/' | sort >"$TMP_PIP_FREEZE"
 
 if ! diff -u "$TMP_REQUIREMENTS" "$TMP_PIP_FREEZE"; then
